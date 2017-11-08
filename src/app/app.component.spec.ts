9aa54237e432d75752650628ab15aaf5e2f08b2c
marklishman <mark.lishman@gmail.com>
2017-11-08 08:36:18 +0000

    Version 5

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,34 @@
 import { TestBed, async } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
+import { routing } from './app.routes';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+import { BlueComponentList } from './blue/blue.component-list';
+import { GreenComponentList } from './green/green.component-list';
+import { RedComponentList } from './red/red.component-list';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
+      imports: [
+        BrowserModule,
+        routing
       ],
+      declarations: [
+        AppComponent,
+        ...RedComponentList,
+        ...GreenComponentList,
+        ...BlueComponentList
+      ],
+      providers: [
+        {provide: LocationStrategy, useClass: HashLocationStrategy}
+      ]
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+  it(`should have some tests`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    // test here
   }));
 });
---
+++ src/app/red/medium-red.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  template: `
+    <div class="medium-red">
+      <h3>Medium</h3>
+    </div>`,
+  styles: ['.medium-red {background-color: #ff8080;}']
+})
+export class MediumRedComponent {
+}
